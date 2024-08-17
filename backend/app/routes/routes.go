c9156d30d0961a715b4166be21f197dbcd347611
arnold.adero <arnold.adero@hotmail.com>
2024-08-18 02:20:12 +0300

    Feat : Define rout for sauti creation

---
+++ backend/app/routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+    "net/http"
+    "sautii-process/backend/controllers"
+
+    "github.com/gorilla/mux"
+)
+
+// RegisterRoutes registers all application routes
+func RegisterRoutes(router *mux.Router) {
+    // Issue creation route
+    router.HandleFunc("/api/issues", controllers.CreateIssue).Methods("POST")
+
+    // Additional routes can be added here...
+}
